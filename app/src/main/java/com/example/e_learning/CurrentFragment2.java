9fb8c6f98fb1095c474a6dcb95effd7970e6a67a
zkd <17301146@bjtu.edu.cn>
2019-12-31 16:48:16 +0800

    E-learning third homework

--- app/src/main/java/com/example/e_learning/CurrentFragment2.java
+++ app/src/main/java/com/example/e_learning/CurrentFragment2.java
@@ -1,6 +1,7 @@
 package com.example.e_learning;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,17 +13,26 @@ import androidx.fragment.app.Fragment;
 public class CurrentFragment2 extends Fragment {
 
     private ImageView my_course1_go;
+    private ImageView my_course2_go;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_my_study, container,false);
 
         my_course1_go = (ImageView) v.findViewById(R.id.my_course1_go);
+        my_course2_go = (ImageView) v.findViewById(R.id.my_course2_go);
 
         my_course1_go.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(), ResourceActivity.class));
+            }
+        });
+
+        my_course2_go.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), WebResourceActivity.class));
             }
         });
 
--- app/src/main/java/com/example/e_learning/WebResourceActivity.java
+++ app/src/main/java/com/example/e_learning/WebResourceActivity.java
@@ -1,17 +1,13 @@
 package com.example.e_learning;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class ResourceActivity extends AppCompatActivity {
+public class WebResourceActivity extends AppCompatActivity {
 
     private Button download_button1;
     private Button download_button2;
@@ -19,19 +15,19 @@ public class ResourceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resource);
+        setContentView(R.layout.activity_web_resource);
 
         download_button1 = (Button) findViewById(R.id.download_button1);
         download_button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Toast.makeText(ResourceActivity.this, "下载成功，存储在文件管理Movies文件夹", Toast.LENGTH_SHORT).show();
+                Toast.makeText(WebResourceActivity.this, "下载成功，存储在文件管理Movies文件夹", Toast.LENGTH_SHORT).show();
             }
         });
 
         download_button2 = (Button) findViewById(R.id.download_button2);
         download_button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Toast.makeText(ResourceActivity.this, "下载成功，存储在文件管理Movies文件夹", Toast.LENGTH_SHORT).show();
+                Toast.makeText(WebResourceActivity.this, "下载成功，存储在文件管理Movies文件夹", Toast.LENGTH_SHORT).show();
             }
         });
     }
